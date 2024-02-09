1cc2d5fc8a66878858421036cceb4529e5820d07
Nirmean <ana_noona@live.com>
2024-02-09 19:01:16 +0000

    array of manager queations created

--- starter/index.js
+++ starter/index.js
@@ -4,6 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const htmlFile = require(".starter/index.html")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,3 +14,47 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?",
+        validate: function(value) {
+            const valid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+            return valid || 'Please enter a valid email address.';
+        },
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+    },
+]
+
+async function createManager() {
+
+}
+
+function addTeamMembers() {
+
+}
+
+function createEngineer() {
+
+}
+
+function createIntern() {
+
+}
+
+function renderHTML() {}
